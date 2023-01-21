bd8cfc2935169c4cd832a564c185b58ef4b83ba4
Angela Caldas <aes.caldas@gmail.com>
2023-01-21 12:40:56 -0300

    feat: states with class components

--- 1_react_basics/src/App.js
+++ 1_react_basics/src/App.js
@@ -1,25 +1,56 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // declaring props in class components
+  constructor(props) {
+    super(props);
+    // binding the method to the component
+    this.handlePClick = this.handlePClick.bind(this);
+    this.state = {
+      name: 'Angela Caldas',
+      counter: 0
+    };
+  }
+
+  handlePClick() {
+    // to access 'this' inside the method we have to bind it in the constructor
+    this.setState({ name: 'Dev Júnior' });
+  }
+
+  // we can use a method without 'bind' with arrow functions. Arrow functions don't have 'this'. So if we use 'this', the function will look for it in other parts of the component
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    // using states with class components
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o Link
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
