15231c04dd9ae037b690a3fc4e491bd37175c7db
Angela Caldas <aes.caldas@gmail.com>
2023-01-21 13:08:57 -0300

    chore: create and practice lifecycle methods with class components

--- 1_react_basics/src/App.js
+++ 1_react_basics/src/App.js
@@ -1,51 +1,70 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  // declaring props in class components
-  // remove binding and use arrow functions on method for cleaner code
-  // this.handlePClick = this.handlePClick.bind(this);
-
-  // removing constructor and use public class states for cleaner code
+  // state as arrays and objects
   state = {
-    name: 'Angela Caldas',
-    counter: 0
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O Título 1',
+        body: 'O Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O Título 2',
+        body: 'O Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O Título 3',
+        body: 'O Corpo 3'
+      },
+    ]
   };
 
-  handlePClick = () => {
-    this.setState({ name: 'Dev Júnior' });
-  };
+  timeoutUpdate = null;
 
-  // we can use a method without 'bind' with arrow functions. Arrow functions don't have 'this'. So if we use 'this', the function will look for it in other parts of the component
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  // lifecycle methods:
+  // DidMount does something ONCE after the component is rendered. Good for fetching API's
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  // DidUpdate does something after the component is updated. This example will create an infinite loop with the counter, because every time counter changes, it will be incremented and DidUpdate will be called again
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  // depending on the function we have on the update, the component may try to continue to update even it it's unmounted, creating errors... to clean up the code, we can use WillUnmount to clear our Timeout (which is happening in our example)
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O título mudou!';
+
+    // setTimeout only to simulate a fetching time
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 2000);
   };
 
   render() {
-    // using states with class components
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o Link
-          </a>
-        </header>
+        <h2>{counter}</h2>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
