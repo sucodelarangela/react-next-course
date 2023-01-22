689d42f7ae53ab8df96adece38ce7eb8940b3ae4
Angela Caldas <aes.caldas@gmail.com>
2023-01-22 13:30:00 -0300

    feat: fetch data from json placeholder and create info cards

--- 1_react_basics/src/App.js
+++ 1_react_basics/src/App.js
@@ -5,67 +5,50 @@ import './App.css';
 class App extends Component {
   // state as arrays and objects
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O Título 1',
-        body: 'O Corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O Título 2',
-        body: 'O Corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O Título 3',
-        body: 'O Corpo 3'
-      },
-    ]
+    posts: []
   };
-
-  timeoutUpdate = null;
 
   // lifecycle methods:
   // DidMount does something ONCE after the component is rendered. Good for fetching API's
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
   }
 
-  // DidUpdate does something after the component is updated. This example will create an infinite loop with the counter, because every time counter changes, it will be incremented and DidUpdate will be called again
-  componentDidUpdate() {
-    this.handleTimeout();
-  }
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  // depending on the function we have on the update, the component may try to continue to update even it it's unmounted, creating errors... to clean up the code, we can use WillUnmount to clear our Timeout (which is happening in our example)
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  }
+    // usaremos Promise.all para resolver com await os fetches acima
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'O título mudou!';
+    const postsJson = await posts.json(); // 100 posts
+    const photosJson = await photos.json(); // 5000 imgs
 
-    // setTimeout only to simulate a fetching time
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 2000);
+    // 'zipping' to unite only 100 imgs to the 100 posts
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: postsAndPhotos });
   };
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h2>{counter}</h2>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post" key={post.id}>
+              <img src={post.cover} alt={post.title} />
+              <div className='post-content'>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
