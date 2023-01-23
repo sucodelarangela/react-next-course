84d0d949d8a9a03da42a1c040f4780cf4e6111a6
Angela Caldas <aes.caldas@gmail.com>
2023-01-23 12:44:50 -0300

    refactor: converting class components to functional components

--- 1_react_basics/src/components/Button/index.jsx
+++ 1_react_basics/src/components/Button/index.jsx
@@ -1,14 +1,21 @@
 import './styles.css';
 
-import { Component } from "react";
-
+export const Button = ({ text, clickAction, disabled }) => (
+  <button
+    onClick={clickAction}
+    className='button'
+    disabled={disabled}
+  >
+    {text}
+  </button>
+);
 // class component just for learning purposes
-export class Button extends Component {
-  render() {
-    const { text, clickAction, disabled } = this.props;
+// export class Button extends Component {
+//   render() {
+//     const { text, clickAction, disabled } = this.props;
 
-    return (
-      <button onClick={clickAction} className='button' disabled={disabled}>{text}</button>
-    );
-  }
-}
+//     return (
+//       <button onClick={clickAction} className='button' disabled={disabled}>{text}</button>
+//     );
+//   }
+// }
--- 1_react_basics/src/templates/Home/index.jsx
+++ 1_react_basics/src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import './styles.css';
 
@@ -7,42 +7,35 @@ import { loadPosts } from '../../utils/load-posts';
 import { Button } from '../../components/Button';
 import { SearchInput } from '../../components/SearchInput';
 
-class Home extends Component {
-  // state as arrays and objects
-  state = {
-    posts: [], // get only part of the posts for pagination
-    allPosts: [], // get all posts
-    page: 0,
-    postsPerPage: 9,
-    searchValue: ''
-  };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(9);
+  const [searchValue, setSearchValue] = useState('');
 
-  // lifecycle methods:
-  // DidMount does something ONCE after the component is rendered. Good for fetching API's
-  componentDidMount() {
-    this.loadPosts();
-  }
+  const noMorePosts = page + postsPerPage >= allPosts.length;
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  const filteredPosts = !!searchValue ?
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    :
+    posts;
 
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
 
     // setting pagination
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos
-    });
-  };
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-  loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
 
+  const loadMorePosts = () => {
     // find next page for pagination
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
@@ -51,55 +44,141 @@ class Home extends Component {
     posts.push(...nextPosts);
 
     // set posts state and new value to page
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   };
 
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    const filteredPosts = !!searchValue ?
-      allPosts.filter(post => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-      :
-      posts;
-
-    return (
-      <section className='container'>
-        <div className="search-container">
-          {/* '!!' converts to boolean */}
-          {!!searchValue && (
-            <h2>Search value: {searchValue}</h2>
-          )}
-
-          <SearchInput searchValue={searchValue} handleChange={this.handleChange} />
-        </div>
-
-        {filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
+  return (
+    <section className='container'>
+      <div className="search-container">
+        {/* '!!' converts to boolean */}
+        {!!searchValue && (
+          <h2>Search value: {searchValue}</h2>
         )}
 
-        {filteredPosts.length === 0 && (
-          <p>Não existem posts.</p>
-        )}
+        <SearchInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
 
-        <div className="button-container">
-          {!searchValue && (
-            <Button
-              text='Load more posts'
-              clickAction={this.loadMorePosts}
-              disabled={noMorePosts}
-            />
-          )}
-        </div>
-      </section>
-    );
-  }
-}
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>Não existem posts.</p>
+      )}
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text='Load more posts'
+            clickAction={loadMorePosts}
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+  );
+
+};
+
+// class Home2 extends Component {
+//   // state as arrays and objects
+//   state = {
+//     posts: [], // get only part of the posts for pagination
+//     allPosts: [], // get all posts
+//     page: 0,
+//     postsPerPage: 9,
+//     searchValue: ''
+//   };
+
+//   // lifecycle methods:
+//   // DidMount does something ONCE after the component is rendered. Good for fetching API's
+//   componentDidMount() {
+//     this.loadPosts();
+//   }
+
+//   loadPosts = async () => {
+//     const { page, postsPerPage } = this.state;
+
+//     const postsAndPhotos = await loadPosts();
+
+//     // setting pagination
+//     this.setState({
+//       posts: postsAndPhotos.slice(page, postsPerPage),
+//       allPosts: postsAndPhotos
+//     });
+//   };
+
+//   loadMorePosts = () => {
+//     const {
+//       page,
+//       postsPerPage,
+//       allPosts,
+//       posts
+//     } = this.state;
+
+//     // find next page for pagination
+//     const nextPage = page + postsPerPage;
+//     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+//     // add nextPosts to posts state
+//     posts.push(...nextPosts);
+
+//     // set posts state and new value to page
+//     this.setState({ posts, page: nextPage });
+//   };
+
+//   handleChange = (e) => {
+//     const { value } = e.target;
+//     this.setState({ searchValue: value });
+//   };
+
+//   render() {
+//     const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+//     const noMorePosts = page + postsPerPage >= allPosts.length;
+//     const filteredPosts = !!searchValue ?
+//       allPosts.filter(post => {
+//         return post.title.toLowerCase().includes(searchValue.toLowerCase());
+//       })
+//       :
+//       posts;
+
+//     return (
+//       <section className='container'>
+//         <div className="search-container">
+//           {/* '!!' converts to boolean */}
+//           {!!searchValue && (
+//             <h2>Search value: {searchValue}</h2>
+//           )}
+
+//           <SearchInput searchValue={searchValue} handleChange={this.handleChange} />
+//         </div>
+
+//         {filteredPosts.length > 0 && (
+//           <Posts posts={filteredPosts} />
+//         )}
+
+//         {filteredPosts.length === 0 && (
+//           <p>Não existem posts.</p>
+//         )}
+
+//         <div className="button-container">
+//           {!searchValue && (
+//             <Button
+//               text='Load more posts'
+//               clickAction={this.loadMorePosts}
+//               disabled={noMorePosts}
+//             />
+//           )}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
 
 export default Home;
