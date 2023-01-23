c52bf5a75895936be937b417f25408621c52da58
Angela Caldas <aes.caldas@gmail.com>
2023-01-23 14:04:51 -0300

    test: test Button component

---
+++ 1_react_basics/src/templates/Home/Home.spec.jsx
@@ -0,0 +1,7 @@
+describe('<Home />', () => {
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  });
+});
+
+// Usually we use 'spec' on test files for unit tests and 'test' for integration tests
