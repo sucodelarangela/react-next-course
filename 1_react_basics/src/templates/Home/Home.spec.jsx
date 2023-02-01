0168e45bbaebdbbc272f542a54d1c87882e9d7a5
Angela Caldas <aes.caldas@gmail.com>
2023-02-01 14:02:41 -0300

    test: mock service worker for fetch requisitions

--- 1_react_basics/src/templates/Home/Home.spec.jsx
+++ 1_react_basics/src/templates/Home/Home.spec.jsx
@@ -1,6 +1,79 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import Home from '.';
+
+// creating handlers to intercept requisitions from front end with mock-service-workers. In this case, the handler will intercept the real data from the url and return this mock data.
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title 1',
+          body: 'body 1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title 2',
+          body: 'body 2',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title 3',
+          body: 'body 3',
+        },
+      ]),
+    );
+  }),
+  rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          url: 'img1.jpg',
+        },
+        {
+          url: 'img2.jpg',
+        },
+        {
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  // before all tests, listen to the server
+  beforeAll(() => {
+    server.listen();
+  });
+
+  // after all tests, closes the server connection
+  afterAll(() => {
+    server.close();
+  });
+
+  // after each test, reset the handlers
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+
+    // noMorePosts is briefly rendered in the screen before data fetching finishes...
+    const noMorePosts = screen.getByText('Não existem posts.');
+
+    // ...using async methods to verify if noMorePosts disappears from the screen after fetching finishes. App must be running for fetch to work.
+    await waitForElementToBeRemoved(noMorePosts);
+
+    screen.debug();
   });
 });
 
